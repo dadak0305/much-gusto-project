c0edc3404b0afea4fd321330a898d7bdc587818d
dadak0305 <dub18058@gmail.com>
2025-10-29 19:45:05 +0900

    Update 00_test.py

--- pages/00_test.py
+++ pages/00_test.py
@@ -6,7 +6,7 @@ import pandas as pd
 # https://raw.githubusercontent.com/huhshin/streamlit/master/data_iris.csv 읽고 나타내기 
 def main_page():
     st.header('Main Page')
-    st.image('https://images.pexels.com/photos/5677011/pexels-photo-5677011.jpeg?auto=compress&cs=tinysrgb&w=200')
+    st.image('https://i.namu.wiki/i/bRquAYJGh2tiaYVDDOXz3VlHMVNTXr_joON-LFwS22CFOu_aZcyl8DCKmaLVbBB5RNNCw5aWGvTLHkVgNEKqjw.svg')
     iris = pd.read_csv('https://raw.githubusercontent.com/huhshin/streamlit/master/data_iris.csv')
     st.write(iris)
     
