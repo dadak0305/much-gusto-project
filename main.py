7c3c48375e2a7608cfc6e61b1dd44c294a2ab8fc
dadak0305 <dub18058@gmail.com>
2025-10-29 19:33:41 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+import streamlit as st
+st.title('hola 나이 첫 웹사이트!!')
+name = st.text_input('당신의 이름 : ')
+menu = st.selectbox('좋아하는 나라을 선택해주세요:', ['스페인','미국'])
+if st.button('인사말 생성') : 
+  st.write(name+'님! 당신이 좋아하는 음식은 '+menu+'이군요?! 저도 좋아요!!')
